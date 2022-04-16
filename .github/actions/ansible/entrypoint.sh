4c39de3631b2bc78ef32e823de36348cbcc23c0a
meet patel <meet@meets-MacBook-Air.local>
2022-04-16 11:49:34 +0530

    entrypoint code updated

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -7,5 +7,4 @@ echo "This is the secret: $SSH_PASSWORD"
 echo "ansible_ssh_pass=$SSH_PASSWORD" >> /hosts
 echo "ansible_become_pass=$SSH_PASSWORD" >> /hosts
 
-# ansible-playbook ansible/playbook.yml --user ubuntu
-ansible-playbook ansible/playbook.yml -i 54.242.222.3 --user ubuntu
+ansible-playbook ansible/playbook.yml --user ubuntu
